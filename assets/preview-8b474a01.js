f719d51e723286e0c1a923ae3c41e27c8bf455d9
peal-26 <edilasio@live.com>
2023-10-05 12:20:09 +0100

    Updates

---
+++ assets/preview-8b474a01.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-e9acb9d0.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-82c50176.js"),["./DocsRenderer-3PUGWF3O-82c50176.js","./iframe-e9acb9d0.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-38063e1e.js","./index-ffc7e5ff.js","./index-e82d7a9f.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-8b474a01.js.map
