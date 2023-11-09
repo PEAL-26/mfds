a6f1bf69c9091c5127c1064ca5715cd70764b049
peal-26 <edilasio@live.com>
2023-11-09 01:00:15 +0100

    path font

--- src/assets/fonts/index.ts
+++ src/assets/fonts/index.ts
@@ -3,66 +3,66 @@ import localFont from 'next/font/local';
 export const fonts = {
   KoHo: localFont({
     src: [
-      {
-        path: './KoHo-Bold.ttf',
-        weight: '700',
-        style: 'normal',
-      },
-      {
-        path: './KoHo-BoldItalic.ttf',
-        weight: '700',
-        style: 'italic',
-      },
-      {
-        path: './KoHo-ExtraLight.ttf',
-        weight: '200',
-        style: 'normal',
-      },
-      {
-        path: './KoHo-ExtraLightItalic.ttf',
-        weight: '200',
-        style: 'italic',
-      },
-      {
-        path: './KoHo-Italic.ttf',
-        weight: '400',
-        style: 'italic',
-      },
-      {
-        path: './KoHo-Light.ttf',
-        weight: '300',
-        style: 'normal',
-      },
-      {
-        path: './KoHo-LightItalic.ttf',
-        weight: '300',
-        style: 'italic',
-      },
-      {
-        path: './KoHo-Medium.ttf',
-        weight: '500',
-        style: 'normal',
-      },
-      {
-        path: './KoHo-MediumItalic.ttf',
-        weight: '500',
-        style: 'italic',
-      },
-      {
-        path: './KoHo-Regular.ttf',
-        weight: '400',
-        style: 'normal',
-      },
-      {
-        path: './KoHo-SemiBold.ttf',
-        weight: '600',
-        style: 'normal',
-      },
-      {
-        path: './KoHo-SemiBoldItalic.ttf',
-        weight: '600',
-        style: 'italic',
-      },
+      // {
+      //   path: './KoHo-Bold.ttf',
+      //   weight: '700',
+      //   style: 'normal',
+      // },
+      // {
+      //   path: './KoHo-BoldItalic.ttf',
+      //   weight: '700',
+      //   style: 'italic',
+      // },
+      // {
+      //   path: './KoHo-ExtraLight.ttf',
+      //   weight: '200',
+      //   style: 'normal',
+      // },
+      // {
+      //   path: './KoHo-ExtraLightItalic.ttf',
+      //   weight: '200',
+      //   style: 'italic',
+      // },
+      // {
+      //   path: './KoHo-Italic.ttf',
+      //   weight: '400',
+      //   style: 'italic',
+      // },
+      // {
+      //   path: './KoHo-Light.ttf',
+      //   weight: '300',
+      //   style: 'normal',
+      // },
+      // {
+      //   path: './KoHo-LightItalic.ttf',
+      //   weight: '300',
+      //   style: 'italic',
+      // },
+      // {
+      //   path: './KoHo-Medium.ttf',
+      //   weight: '500',
+      //   style: 'normal',
+      // },
+      // {
+      //   path: './KoHo-MediumItalic.ttf',
+      //   weight: '500',
+      //   style: 'italic',
+      // },
+      // {
+      //   path: './KoHo-Regular.ttf',
+      //   weight: '400',
+      //   style: 'normal',
+      // },
+      // {
+      //   path: './KoHo-SemiBold.ttf',
+      //   weight: '600',
+      //   style: 'normal',
+      // },
+      // {
+      //   path: './KoHo-SemiBoldItalic.ttf',
+      //   weight: '600',
+      //   style: 'italic',
+      // },
     ],
   }),
 };
