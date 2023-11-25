c05078345985d2f20f79aa1610c9b610b8adf0d4
peal-26 <edilasio@live.com>
2023-11-25 11:42:09 +0100

    add scroll area

--- src/libs/lottie-react.tsx
+++ src/libs/lottie-react.tsx
@@ -1,9 +1,7 @@
 import { NotFound } from './lottie-react/animations';
 import { LottieReact } from './lottie-react/index';
 
-export const Animations = {
-  NotFound,
-};
+export const Animations = { NotFound };
 
 export * from './lottie-react/index';
 export default LottieReact;
