4b91ada96f359caa05d6c393f4613fb08aba4a1b
peal-26 <edilasio@live.com>
2024-02-17 18:49:03 +0100

    datepicker disable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { DateTimePicker } from "software-edv-design-system/components";
 export default function App() {
   return (
     <>
+      <DateTimePicker />
       <h1 className="text-3xl font-bold underline text-red-400">
         Hello world!
       </h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "software-edv-design-system/components"
+"use client";
+// import { Button, DateTimePicker } from "software-edv-design-system/components"
+import { DateTimePicker } from "../../../design-system/components";
 
 export default function Page() {
   return (
     <>
-      <Button.Root variant="primary" >Botão padrão</Button.Root> 
+      {/* <Button.Root variant="primary" >Botão padrão</Button.Root>  */}
+      <DateTimePicker />
+      <DateTimePicker
+        startDate={new Date("2024-02-04")}
+        endDate={new Date("2024-02-17")}
+      />
       <h1>Examples Design System</h1>
     </>
   );
