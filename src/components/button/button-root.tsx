0b4a61fe7005eec2e0407673dccc447e9b32ef6d
peal-26 <edilasio@live.com>
2024-02-17 18:56:40 +0100

    add ref in button

--- src/components/button/button-root.tsx
+++ src/components/button/button-root.tsx
@@ -1,13 +1,16 @@
+import { forwardRef } from 'react';
 import { Button, ButtonProps } from './button';
 
 import { twMerge } from 'tailwind-merge';
 
-export function ButtonRoot(props: ButtonProps) {
+export const ButtonRoot = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   const { children, className, ...rest } = props;
 
   return (
-    <Button className={twMerge('flex items-center gap-2', className)} {...rest}>
+    <Button className={twMerge('flex items-center gap-2', className)} {...rest} ref={ref}>
       {children}
     </Button>
   );
-}
+});
+
+ButtonRoot.displayName = 'ButtonRoot';
--- src/components/button/button/button.tsx
+++ src/components/button/button/button.tsx
@@ -1,16 +1,24 @@
 import { twMerge } from 'tailwind-merge';
 
+import { forwardRef } from 'react';
 import { ButtonProps } from './types';
 import { buttonVariants } from './variants';
 
-export function Button(props: ButtonProps) {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   const { children, type = 'button', variant = 'primary', className, ...rest } = props;
 
   const classNameVariant = buttonVariants({ variant });
 
   return (
-    <button type={type} className={twMerge(`text-center ${classNameVariant}`, className)} {...rest}>
+    <button
+      ref={ref}
+      type={type}
+      className={twMerge(`text-center ${classNameVariant}`, className)}
+      {...rest}
+    >
       {children}
     </button>
   );
-}
+});
+
+Button.displayName = 'Button';
