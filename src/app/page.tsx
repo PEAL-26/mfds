22990b60dad673171acc381819e9408299214874
peal-26 <edilasio@live.com>
2024-04-14 19:57:03 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
-// import { Button, DateTimePicker } from "software-edv-design-system/components"
 import { DateTimePicker } from "../../../design-system/components";
+import { CheckBox } from "../../../design-system/components";
 
 export default function Page() {
   return (
-    <>
-      {/* <Button.Root variant="primary" >Botão padrão</Button.Root>  */}
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="mb-10">Examples Design System</h1>
+      <CheckBox onChecked={(state) => console.log(state)} />
       <DateTimePicker />
       <DateTimePicker
         startDate={new Date("2024-02-04")}
         endDate={new Date("2024-02-17")}
       />
-      <h1>Examples Design System</h1>
-    </>
+    </div>
   );
 }
