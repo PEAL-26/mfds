3417bcdb0737a2f7906c6a97c9751da022bf91bc
peal-26 <edilasio@live.com>
2024-04-14 17:56:38 +0100

    create onChecked check-box

--- src/components/check-box/index.tsx
+++ src/components/check-box/index.tsx
@@ -5,13 +5,17 @@ import { CheckBoxProps } from './types';
 import { checkBoxVariants } from './variants';
 
 export const CheckBox = forwardRef<HTMLInputElement, CheckBoxProps>((props: CheckBoxProps, ref) => {
-  const { children, variant = 'primary', ...rest } = props;
+  const { children, variant = 'primary', onChecked, onChange, ...rest } = props;
   const classNameVariant = checkBoxVariants({ variant });
 
   return (
     <input
       ref={ref}
       type="checkbox"
+      onChange={(e) => {
+        onChange(e);
+        onChecked?.(e.currentTarget.checked);
+      }}
       {...rest}
       className={twMerge(classNameVariant, rest.className)}
     />
--- src/components/check-box/types.ts
+++ src/components/check-box/types.ts
@@ -4,4 +4,5 @@ export type CheckBoxVariantTypes = 'primary' | 'success' | 'error' | 'none';
 
 export interface CheckBoxProps extends HTMLProps<HTMLInputElement> {
   variant?: CheckBoxVariantTypes;
+  onChecked?: (checked: boolean) => void;
 }
