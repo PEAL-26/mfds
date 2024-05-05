909d093cdcf4f1e2a010eff35f099fd41ff2fc19
peal-26 <edilasio@live.com>
2024-05-05 16:08:05 +0100

    feat: Error variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,6 +40,12 @@ export default function Page() {
     <div className="flex flex-col justify-center items-center min-h-screen p-10">
       <h1 className="mb-10 ">Examples Design System</h1>
       <div className="flex flex-col gap-3 justify-start items-start">
+        <Input.WithLabel
+          label="Label"
+          error="Error"
+          type="money"
+          variant="primary"
+        />
         <Input.Text type="money" variant="primary" />
         <Input.Text type="literal" variant="primary" />
         <Input.Text type="number" variant="primary" />
