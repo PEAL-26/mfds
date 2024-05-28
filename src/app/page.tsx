c47eae14c022295d75b4c637f0d4f7fa95ddfdc3
peal-26 <edilasio@live.com>
2024-05-28 08:23:44 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { z } from "zod";
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import {
   Select,
   SelectSearch,
@@ -10,13 +13,39 @@ import {
   CheckToggle,
   UploadFileSelector,
 } from "../../../design-system/components";
-import { useForm, FormProvider } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+
+// const MAX_FILE_SIZE = 5000000;
+// const ACCEPTED_IMAGE_TYPES = [
+//   "image/jpeg",
+//   "image/jpg",
+//   "image/png",
+//   "image/webp",
+// ];
+
+const MAX_UPLOAD_SIZE = 1024 * 1024 * 1; // 3MB
+const ACCEPTED_FILE_TYPES = ["image/png"];
 
 const schema = z.object({
   country: z.object({
     id: z.string(),
     name: z.string(),
+    // image: z
+    //   .any()
+    //   .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+    //   .refine(
+    //     (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+    //     "Only .jpg, .jpeg, .png and .webp formats are supported."
+    //   ),
+    image: z
+      .any()
+      .optional()
+      .refine((file) => {
+        return !file || file.size <= MAX_UPLOAD_SIZE;
+      }, "File size must be less than 3MB")
+      .refine((file) => {
+        console.log(file);
+        return ACCEPTED_FILE_TYPES.includes(String(file?.type));
+      }, "File must be a PNG"),
   }),
 });
 
@@ -35,7 +64,7 @@ export default function Page() {
     resolver: zodResolver(schema),
   });
   const onSubmit = (data: SchemaType) => console.log(data);
-  console.log(form.watch("country")); // watch input value by passing the name of it
+  console.log(form.watch()); // watch input value by passing the name of it
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen p-10">
@@ -157,6 +186,8 @@ export default function Page() {
             fieldValue="id"
             fieldLabel="name"
           />
+
+          <UploadFileSelector form={form} name="image" />
 
           <input type="submit" />
         </form>
