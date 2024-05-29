5c507e9552f4347c7b7927de35d64430db745d32
peal-26 <edilasio@live.com>
2024-05-29 21:10:48 +0100

    variant button link

--- src/components/button/button/types.ts
+++ src/components/button/button/types.ts
@@ -1,6 +1,13 @@
 import { ButtonHTMLAttributes } from 'react';
 
-export type ButtonVariantTypes = 'primary' | 'secondary' | 'success' | 'error' | 'outline' | 'none';
+export type ButtonVariantTypes =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'error'
+  | 'outline'
+  | 'link'
+  | 'none';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariantTypes;
--- src/components/button/button/variants.ts
+++ src/components/button/button/variants.ts
@@ -11,7 +11,9 @@ export const buttonVariants = tv({
         'rounded-md bg-green-600 text-white py-2 px-3 text-sm font-bold hover:bg-green-700 hover:text-white border border-green-600 hover:border-green-700 transition-all',
       error:
         'rounded-md bg-red-600 text-white py-2 px-3 text-sm font-bold hover:bg-red-700 hover:text-white border border-red-600 hover:border-red-700 transition-all',
-      outline: 'rounded-md py-2 px-3 border border-gray-light bg-white hover:bg-gray-50 transition-all',
+      outline:
+        'rounded-md py-2 px-3 border border-gray-light bg-white hover:bg-gray-50 transition-all',
+      link: 'inline-flex w-fit items-center text-blue-600 hover:underline',
       none: '',
     },
   },
