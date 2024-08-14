830cdd56e22031419726cefdb9afb8c2052982c3
peal-26 <edilasio@live.com>
2024-08-14 21:24:41 +0100

    Preview

--- src/app/uploads/page.tsx
+++ src/app/uploads/page.tsx
@@ -4,7 +4,7 @@ import { UploadFileSelector } from "../../../../design-system/components";
 export default function Uploads() {
   return (
     <div className="flex flex-col gap-3 justify-start items-start">
-      <UploadFileSelector />
+      <UploadFileSelector crossOrigin="anonymous" />
     </div>
   );
 }
