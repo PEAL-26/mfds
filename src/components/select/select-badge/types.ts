bf835496b718319575e76e09d8ed41cd127da03a
peal-26 <edilasio@live.com>
2024-10-10 21:13:28 +0100

    FIX: Select badge modal

--- src/components/select/select-badge/types.ts
+++ src/components/select/select-badge/types.ts
@@ -1,7 +1,9 @@
 import { SelectProps } from '../select';
+import { SelectSearchProps } from '../select-search';
 import { SelectTypes } from '../types';
 
-export interface SelectBadgeProps<T> extends SelectProps<T> {
+
+export interface SelectBadgeProps<T> extends SelectProps<T>, SelectSearchProps<T> {
   badge: string;
   badgeAlign?: 'left' | 'right';
   error?: string;
