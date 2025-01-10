90ee9fc297f2d280df027163b018a6a93a63acdb
peal-26 <edilasio@live.com>
2025-01-10 21:08:29 +0100

    FEAT: add card and radio-group

---
+++ src/components/card/index.tsx
@@ -0,0 +1,11 @@
+import * as Cards from './card';
+export { CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './card';
+
+export const Card = {
+  Root: Cards.Card,
+  Content: Cards.CardContent,
+  Description: Cards.CardDescription,
+  Footer: Cards.CardFooter,
+  Header: Cards.CardHeader,
+  Title: Cards.CardTitle,
+};
